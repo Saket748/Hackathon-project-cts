257eb2ad76c856b7a314477e3cd017b8b8db6308
S <2457257@cognizant.com>
2026-01-19 18:00:25 +0530

    checking again

--- src/main/java/org/mp/Main.java
+++ src/main/java/org/mp/Main.java
@@ -4,6 +4,6 @@ package org.mp;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hii swapna just for checking");
+        System.out.println("khali");
     }
 }
