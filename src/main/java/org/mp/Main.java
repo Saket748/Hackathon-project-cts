8f0acc8c2cd7e0bbfbb187d4082ec4440eacd24b
S <2457257@cognizant.com>
2026-01-20 15:25:34 +0530

    Hi I am pusing

--- src/main/java/org/mp/Main.java
+++ src/main/java/org/mp/Main.java
@@ -4,6 +4,6 @@ package org.mp;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("khali");
+        System.out.println("HI");
     }
 }
