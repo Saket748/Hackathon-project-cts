bd4c72c793054646755fe425262c5e9d70116d04
Yadav <2457256@cognizant.com>
2026-01-23 11:23:12 +0530

    added loggers file and setup loggers on FiltersPage.java

--- src/test/java/org/PagesObject/BasePage.java
+++ src/test/java/org/PagesObject/BasePage.java
@@ -1,4 +1,4 @@
-package Pages;
+package org.Pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
--- src/test/java/org/PagesObject/GiftCardsPage.java
+++ src/test/java/org/PagesObject/GiftCardsPage.java
@@ -1,4 +1,4 @@
-package Pages;
+package org.Pages;
 
 import org.openqa.selenium.JavascriptExecutor;
 
