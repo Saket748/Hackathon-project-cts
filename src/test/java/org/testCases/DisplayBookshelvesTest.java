2ebbbcb6a88eac02829284a5c79eabf1cbe4edb9
Jahnavi Dasari <109059174+jahnavinssdasari@users.noreply.github.com>
2026-01-28 10:44:57 +0530

    Refactor DisplayBookshelvesTest to extend BaseClass

--- src/test/java/org/testCases/DisplayBookshelvesTest.java
+++ src/test/java/org/testCases/DisplayBookshelvesTest.java
@@ -1,76 +1,23 @@
 package org.testCases;
 
-import org.openqa.selenium.WebDriver;
-
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import org.testng.annotations.AfterClass;
-
-import org.testng.annotations.BeforeClass;
-
+import org.PagesObject.HomePage;
+import org.PagesObject.FiltersPage;
+import org.testBase.BaseClass;
 import org.testng.annotations.Test;
 
-import org.PagesObject.HomePage;
-
-import org.PagesObject.FiltersPage;
-
-public class DisplayBookshelvesTest {
-
-    WebDriver driver;
-
-    HomePage homePage;
-
-    FiltersPage filtersPage;
-
-    @BeforeClass
-
-    public void setup() {
-
-        // Launch browser (NOT headless)
-
-        driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-
-        driver.get("https://www.urbanladder.com");
-
-        homePage = new HomePage(driver);
-
-        filtersPage = new FiltersPage(driver);
-
-    }
+public class DisplayBookshelvesTest extends BaseClass {
 
     @Test
+    public void verify_bookshelves_below_15000_open_storage() {
+        // 1) Navigate to Bookshelves page
+        HomePage homePage = new HomePage(driver);
+        homePage.openBookshelves();
 
-    public void displayBookshelves() {
+        // 2) Apply Open Storage filter
+        FiltersPage filtersPage = new FiltersPage(driver);
+        filtersPage.applyOpenStorageFilter();
 
-        // Step 1: Search Bookshelves under 15000
-
-        homePage.searchItem("Bookshelves under 15000");
-
-        // Step 2: Apply Filters
-
-        filtersPage.openAllFilters();
-
-        filtersPage.selectStorageType();
-
-        filtersPage.selectOpenStorage();
-
-        filtersPage.applyFilters();
-
-        // Step 3: Get first 3 products
-
-        filtersPage.getFirstThreeProducts();
-
+        // 3) Print first 3 products below Rs. 15000 (excluding out of stock)
+        filtersPage.printFirstThreeProducts();
     }
-
-    @AfterClass
-
-    public void tearDown() {
-
-        driver.quit();
-
-    }
-
 }
- 
